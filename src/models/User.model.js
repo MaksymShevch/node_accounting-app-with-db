7be3ac76b2b24324358c12bdcd9608c9208e1532
MaksymShevch <gcrazy007@gmail.com>
2025-03-29 23:06:50 +0200

    add task solution

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -1,9 +1,23 @@
 'use strict';
 
 const { sequelize } = require('../db.js');
+const { DataTypes } = require('sequelize');
 
 const User = sequelize.define(
-  // your code goes here
+  'User',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  },
+  { tableName: 'users', createdAt: false, updatedAt: false },
 );
 
 module.exports = {
